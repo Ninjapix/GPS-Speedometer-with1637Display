8ea27bbc134e8d03d42cb19cb98758510793185a
Patrick M <patrickhmanson123@gmail.com>
2021-07-26 01:45:20 -0700

    Added initialization for 7 segment display.

--- GPSSpeedometer/GPSSpeedometer.ino
+++ GPSSpeedometer/GPSSpeedometer.ino
@@ -17,6 +17,7 @@ SevenSegmentTM1637 display(PIN_CLK, PIN_DIO);
 //GPS objecs
 TinyGPSPlus gps;
 SoftwareSerial ss(PIN_RX, PIN_TX);
+
 //USER-MADE FUNCTIONS: These are the ones we're coding :)
 void printMPH(double speed){
   //Ben: this function should take in a decimal, and print that out to the display. it should only have 3 digits, formatted xx.x.
@@ -24,21 +25,33 @@ void printMPH(double speed){
 }
 
 double getMPH(){
+  //This function can easily be replaced with just normal code. For some reason the double will output as xx.xx instead of xx.x for decimals, despite the fact that the hundreths is just zeros.
   double tempMPH = gps.speed.mph();
   tempMPH = round(tempMPH*10)/10;
+  //round function gotten from user rastapasta from: https://community.particle.io/t/changing-a-result-74-14505494505494-to-one-decimal-place/7337/2
   return tempMPH;
 }
 
 void startGPS(){
   //Patrick: there's some init for this module but I'm not sure. Gotta do more research
+  //Upate: yeah no there was like no init haha. It's just starting software serial +  normal serial, which can probably be ommitted. Too lazy to see if that's the case or not.
   Serial.begin(9600);
   ss.begin(9600);
+  return;
+}
+void startDisplay(){
+  //serial should be begun now but already doing it in gps init
+  display.begin();
+  display.setBacklight(1000);
+  display.setColonON(true);
   return;
 }
 //ARDINO FUNCTIONS: The board uses these in particular to function. Essential to any and every project.
 void setup() {
   startGPS();
-  //this is used for setting stuff up
+  //this is used for setting stuff up, runs two lines of code so it's just for cleanliness
+  startDisplay();
+  //same as start GPS, just for cleanliness
 }
 
 void loop() {
@@ -46,13 +59,14 @@ void loop() {
   double mph = 0;
   while (ss.available() > 0){
     if (gps.encode(ss.read())){
+      //!!!!!!!!!!!!!MPH EQUALS 98.7 IS JUST FOR TESTING PURPOSES. IT SHOULD NOT BE LIKE THAT IN THE FINAL. THIS IS JUST TO TEST THAT THINGS GET THROUGH.
       mph = 98.7;
       mph = getMPH();
       printMPH(mph);
+      //this can be condensed into printMPH(getMPH()); seperating it because why not lol (^^)
     }
   }
   
-  //round function gotten from user rastapasta from: https://community.particle.io/t/changing-a-result-74-14505494505494-to-one-decimal-place/7337
   
 
 }
