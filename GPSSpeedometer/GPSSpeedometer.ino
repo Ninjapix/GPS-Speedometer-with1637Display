c0d2ea1344b9243ed575a9a176855e61b4903a6b
Patrick M <patrickhmanson123@gmail.com>
2021-07-26 19:13:05 -0700

    Changed code so that the display function accepts an integer, rather than a double. Using a double in testing resulted in the display showing an underscore for the decimal point.

--- GPSSpeedometer/GPSSpeedometer.ino
+++ GPSSpeedometer/GPSSpeedometer.ino
@@ -19,17 +19,22 @@ TinyGPSPlus gps;
 SoftwareSerial ss(PIN_RX, PIN_TX);
 
 //USER-MADE FUNCTIONS: These are the ones we're coding :)
-void printMPH(double speed){
+void printMPH(int speed){
   //Ben: this function should take in a decimal, and print that out to the display. it should only have 3 digits, formatted xx.x.
   //Should return nothing, hence: "void". still needs a return statement. 
 }
 
-double getMPH(){
+int getMPH(){
   //This function can easily be replaced with just normal code. For some reason the double will output as xx.xx instead of xx.x for decimals, despite the fact that the hundreths is just zeros.
   double tempMPH = gps.speed.mph();
-  tempMPH = round(tempMPH*10)/10;
+  tempMPH = round(tempMPH*10);
   //round function gotten from user rastapasta from: https://community.particle.io/t/changing-a-result-74-14505494505494-to-one-decimal-place/7337/2
-  return tempMPH;
+  //round function not ended up being used, as decimals don't play well.
+  int tempMPH2 = tempMPH;
+  if(tempMPH2 > 9999){
+    tempMPH2 = 9999;
+  }
+  return tempMPH2;
 }
 
 void startGPS(){
@@ -43,7 +48,7 @@ void startDisplay(){
   //serial should be begun now but already doing it in gps init
   display.begin();
   display.setBacklight(100);
-  display.setColonON(true);
+  display.setColonOn(true);
   return;
 }
 //ARDINO FUNCTIONS: The board uses these in particular to function. Essential to any and every project.
@@ -56,11 +61,11 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  double mph = 0;
+  int mph = 0;
   while (ss.available() > 0){
     if (gps.encode(ss.read())){
-      //!!!!!!!!!!!!!MPH EQUALS 98.7 IS JUST FOR TESTING PURPOSES. IT SHOULD NOT BE LIKE THAT IN THE FINAL. THIS IS JUST TO TEST THAT THINGS GET THROUGH.
-      mph = 98.7;
+      //!!!!!!!!!!!!!MPH EQUALS 987 IS JUST FOR TESTING PURPOSES. IT SHOULD NOT BE LIKE THAT IN THE FINAL. THIS IS JUST TO TEST THAT THINGS GET THROUGH.
+      mph = 987;
       mph = getMPH();
       printMPH(mph);
       //this can be condensed into printMPH(getMPH()); seperating it because why not lol (^^)
